3c70cb3b2017916651b68ce0d6466f91f2864cd8
R5dM1st <emile.duplais@isen-ouest.yncrea.fr>
2023-05-10 23:40:25 +0200

    that cool

--- function.js
+++ function.js
@@ -16,18 +16,49 @@ function scratchCard() {
 //Un clic sur ce bouton entraîne l’ouverture d’une fenêtre prompt qui demande d’entrer le nom du profil administrateur « admin » si on clique sur le bouton
 function modeEdition(){
     let admin = prompt('Entrez le nom du profil administrateur');
-    if (admin == 'admin'){
-        alert('Vous êtes en mode édition');
-        //on peut modifier la balise name directement depuis le html et la mise à jour en appuiyant sur le bouton
-        let name = document.getElementsByClassName('name');
-        name[0].innerHTML = '<input type="text" name="name" value="Nom" id="p1">';
-        name[1].innerHTML = '<input type="text" name="name" value="Nom" id="p2">';
-        let input = document.getElementById("in").value;
+    if ( admin == 'admin'){
+    let admin_pwd = prompt('Entrez le mot de passe du profil administrateur');
+        if ( admin_pwd == 'admin'){
+            alert('Vous êtes en mode édition');
+            //change de couleur le bouton mode édition
+            let mode = document.getElementsByClassName('modeEdition');
+            mode.style.backgroundColor = 'green';
+            //on peut modifier la balise name directement depuis le html et la mise à jour en appuiyant sur le bouton
+            let name=document.getElementsByClassName('name');
+            
+            name[0].innerHTML = '<input type="text" name="name" value="Nom" id="input1">';
+            name[1].innerHTML = '<input type="text" name="name" value="Nom" id="input2">';
+            let name1=document.getElementById("input1").value;
+            let name2=document.getElementById("input2").value;
+            //click sur entrer pour mettre à jour le nom et enlever le mode édition
+            document.getElementById("input1").addEventListener("keyup", function(event) {
+                if (event.keyCode === 13) {
+                    event.preventDefault();
+                    document.getElementsByClassName('name')[0].innerHTML = name1;
+                    document.getElementsByClassName('name')[1].innerHTML = name2;
+                    alert('Vous n\'êtes plus en mode édition');
+                }
+            
+            });
+            document.getElementById("input2").addEventListener("keyup", function(event) {
+                if (event.keyCode === 13) {
+                    event.preventDefault();
+                    document.getElementsByClassName('name')[0].innerHTML = name1;
+                    document.getElementsByClassName('name')[1].innerHTML = name2;
+                    alert('Vous n\'êtes plus en mode édition');
+                }
 
+            });
+        }
 
-
-        
+            
     } else {
         alert('Vous n\'êtes pas en mode édition');
     }
+}
+//ajoute une carte membre profile-card dans la balise div container 
+function ajouterMembre(){
+    let container = document.getElementById('container');
+    container.innerHTML += '<div class="profile-card"><div class="profile-picture"></div><div class="name">Nom</div><div class="profile-description">Description</div></div>';
+    
 }
