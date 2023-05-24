5c9847e36156d64f7ce439cfbb2d2c3f3f3626d9
PierreZboril-Berteaud <pierre.zboril-berteaud@isen-ouest.yncrea.fr>
2023-05-24 14:05:55 +0200

    a

--- function.js
+++ function.js
@@ -182,9 +182,6 @@ function supprimerMembre(){
 
 
 //Page2 Recherche
-const showButton = document.getElementById('modal-btn');
-const container = document.getElementById('modal-container');
-const closebtn = document.getElementById('close-btn');
 
 
 function copyplagia(){
@@ -198,3 +195,27 @@ copyplagia();
 
 //page 1 accueil
 
+//Page thematique de recherche 
+function container(){
+let showButton = document.getElementById('modal-btn');
+let container = document.getElementById('modal-container');
+let closebtn = document.getElementById('close-btn');
+
+const showButton2 = document.getElementById('modal-btn2');
+const container2 = document.getElementById('modal-container2');
+const closebtn2 = document.getElementById('close-btn2')
+showButton.addEventListener('click', () => {
+    container.showModal(); //ouvre la fenetre modal
+});
+closebtn.addEventListener('click', () => {
+    container.close(); // Ferme la fenetre modal
+});
+
+showButton2.addEventListener('click', () => {
+    container2.showModal(); //ouvre la fenetre modal
+});
+closebtn2.addEventListener('click', () => {
+    container2.close(); // Ferme la fenetre modal
+});
+}
+container();
--- functionpierre.js
+++ functionpierre.js
@@ -31,14 +31,22 @@ copyplagia();*/
     
 }*/
 function changeImage(){
-    const image = document.getElementById('img0');
-    const image1 = document.getElementById('img1');
+    let image = document.getElementById('img0');
+    let image1 = document.getElementById('img1');
+    let image2 = document.getElementById('img2');
+    let image3 = document.getElementById('img3')
 
     image.addEventListener('click', () => {
         image.src = 'https://media.lesechos.com/api/v1/images/view/5c6ba5f93e45462730262d35/1280x720/060723627156-web-tete.jpg'
     });
     image1.addEventListener('click', () => {
-        image1.src ='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Osama_bin_Laden_portrait.jpg/280px-Osama_bin_Laden_portrait.jpg'
+        image1.src ='https://s1.dmcdn.net/v/Tc7AZ1Y1FhqxHBxJb/x720'
+    });
+    image2.addEventListener('click', () => {
+        image2.src="https://blog.cdn.own3d.tv/resize=fit:crop,height:400,width:600/MO4UcY4SR1K6AnShORk4"
+    });
+    image3.addEventListener('click', () => {
+        image3.src="https://pbs.twimg.com/media/Euq-wexWQAABqnP?format=jpg&name=large"
     });
 }
 changeImage();
